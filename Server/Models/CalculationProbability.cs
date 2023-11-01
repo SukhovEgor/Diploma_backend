e7c85cf6cf8d98a4366fbeb9344d435cf774f1f2
Sukhov Egor <sukhov123net@yandex.ru>
2023-11-01 17:49:01 +0700

    Исправлены методы расчета

--- Server/Models/CalculationProbability.cs
+++ Server/Models/CalculationProbability.cs
@@ -10,12 +10,14 @@
         public double IntermediateRelayTime { get; set; }
         public double CircuitBreakerTime { get; set; }
         public double AdditionalTime { get; set; }
+        public double AdditionalUROVTime { get; set; }
         public double InputTime { get; set; }
 
         public double DevMainRelayTime { get; set; }
         public double DevIntermediateRelayTime { get; set; }
         public double DevCircuitBreakerTime { get; set; }
         public double DevAdditionalTime { get; set; }
+        public double DevAdditionalUROVTime { get; set; }
         public double DevInputTime { get; set; }
 
         public int ImplementationQuantity { get; set; }
